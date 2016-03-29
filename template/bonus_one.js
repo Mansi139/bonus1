905cdc431aaa4cf52b78588ee6b170d5a9a18fd9
Mansi139 <Mansi139@users.noreply.github.com>
2016-03-28 22:19:58 -0400

    Added files via upload

--- template/bonus_one.js
+++ template/bonus_one.js
@@ -11,7 +11,35 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
-(function() {
+function magic() {
 	// Magic!
 	console.log('Keepin\'n it clean with an external script!');
-})();
+    var xhttp = new XMLHttpRequest();
+    var type = "all";
+    xhttp.onreadystatechange = function(){
+        var test = xhttp.responseText;
+        temp.trim();
+        console.log(temp);
+        var response = JSON.parse(temp);
+        console.log(response);
+        console.log(response.data.interests);
+
+        var array = response.data.interests.concat(response.data.programming);
+        
+        
+        
+        $("#search").autocomplete({source: array})
+
+    };
+    xhttp.open("GET", "http://www.mattbowytz.com/simple_api.json?data=" + type, true);
+    xhttp.send();
+}
+
+
+function searchSub(){
+      var search = document.getElementById("search").value;
+    console.log(search);
+    
+    window.location.replace("http://www.google.com/search?q=" + search);
+    
+}
